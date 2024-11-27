fb0c2272771774349698703d08b49b933e2ffc3b
Yanet Choque <sol3127g@gmaill.com>
2024-11-27 17:57:06 -0300

    se agrando la imagen de introducción para mobile

--- src/components/layout/Introduction.tsx
+++ src/components/layout/Introduction.tsx
@@ -5,7 +5,7 @@ import { Networks } from "../common/Networks"
 export const Introduction = () => {
     return (
         <div className="flex flex-col w-full m-auto items-center mt-4 mb-8 lm:flex-row gap-7 lm:gap-0 lm:justify-around ">
-            <img src={My} alt="" className="rounded-3xl w-[190px] h-[225px] lg:w-[220px] lg:h-[270px] xl:w-[280px] xl:h-[335px] z-50 lm:mr-10"/>
+            <img src={My} alt="" className="rounded-3xl w-[205px] h-[245px] lm::w-[220px] lm:h-[270px] xl:w-[280px] xl:h-[335px] z-50 lm:mr-10"/>
             <div className="flex flex-col items-center gap-1 xl:gap-1.5">
                 <p className="font-semibold text-[23px] sm:text-[30px] md:text-[32px] xl:text-[42px]">Yanet Soledad Choque Cayo</p>
                 <p className="text-red-300 text-[20px] sm:text-[22px] xl:text-[28px]">Desarrolladora Full Stack</p>
