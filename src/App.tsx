402e683657bf704d41ba91ba213a8222d0dacb83
Yanet Choque <sol3127g@gmaill.com>
2024-11-28 15:18:12 -0300

    traslado del boton modo oscuro/claro de la app al header

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import { useEffect } from 'react';
 import './App.css'
-import { useTheme } from './components/common/useTheme';
 import { Page } from './page/Page';
-import { Moon, Sun } from 'lucide-react';
 
 const App: React.FC = () => {
-  const { theme, toggleTheme } = useTheme();
-
-  useEffect(() => {
-    document.documentElement.classList.remove('light', 'dark');
-    document.documentElement.classList.add(theme);
-  }, [theme]);
 
   return (
     <>
       <Page />
-      <div className="absolute cursor-pointer top-8 z-50 right-6 md:top-7 md:right-[56px] border-2 hover:text-orange-200 border-black hover:border-[#2c1f31] hover:bg-[#2c1f31] dark:border-white dark:hover:border-secondary dark:hover:bg-secondary dark:hover:text-gray-900 rounded-full p-1 sm:p-1.5">
-        {theme === 'dark' ? (
-          <Sun onClick={toggleTheme} className="text-white dark:hover:text-gray-900 w-5 h-5 sm:w-7 sm:h-7"/>
-        ) : (
-          <Moon onClick={toggleTheme} className="w-5 h-5 sm:w-7 sm:h-7 "/>
-        )}
-      </div>
     </>
   )
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { itemsMenu } from "../../data/data-id";
 import { MotionTransition } from "../common/transition-component";
 import { Link } from "react-router-dom";
+import { useTheme } from "../common/useTheme";
+import { Moon, Sun } from "lucide-react";
 
-export const Header = () => {
+export const Header: React.FC = () => {
+    const { theme, toggleTheme } = useTheme();
+  
+    useEffect(() => {
+      document.documentElement.classList.remove('light', 'dark');
+      document.documentElement.classList.add(theme);
+    }, [theme]);
+
     const scrollTo = (id: string) => {
 		const element = document.getElementById(id);
 		if (element) {
@@ -28,6 +38,13 @@ export const Header = () => {
                     </div>
                 </div>
             </header>
+            <div className="absolute cursor-pointer top-[18px] z-50 right-6 md:top-4 md:right-[56px] border-2 hover:text-orange-200 border-black hover:border-[#2c1f31] hover:bg-[#2c1f31] dark:border-white dark:hover:border-secondary dark:hover:bg-secondary dark:hover:text-gray-900 rounded-full p-1 sm:p-1.5">
+            {theme === 'dark' ? (
+            <Sun onClick={toggleTheme} className="text-white dark:hover:text-gray-900 w-5 h-5 sm:w-7 sm:h-7"/>
+            ) : (
+            <Moon onClick={toggleTheme} className="w-5 h-5 sm:w-7 sm:h-7 "/>
+            )}
+        </div>
         </MotionTransition>
     )
 }
